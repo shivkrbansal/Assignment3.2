c9547c71fd131beb85e8493bbdb02b144d023f9f
shivkr <shivkr@Shiv>
2017-08-29 10:16:26 +0530

    Assignment 3.2

---
+++ src/Assignment3Task.java
@@ -0,0 +1,36 @@
+
+
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+public class Assignment3Task {
+ @SuppressWarnings("deprecation")
+ public static void main(String[] args) throws Exception {
+  Configuration conf = new Configuration();
+  Job job = new Job(conf, "AssignmentTask3.1");
+  job.setJarByClass(Assignment3Task.class);
+  
+  job.setOutputKeyClass(Text.class);
+  job.setOutputValueClass(IntWritable.class);
+  job.setMapperClass(Assignment3Mapper.class);
+  job.setNumReduceTasks(0);
+  job.setInputFormatClass(TextInputFormat.class);
+  job.setOutputFormatClass(TextOutputFormat.class);
+ 
+  FileInputFormat.addInputPath(job, new Path(args[0]));
+  FileOutputFormat.setOutputPath(job,new Path(args[1]));
+  
+  Path out=new Path(args[1]);
+  out.getFileSystem(conf).delete(out);
+  
+  job.waitForCompletion(true);
+ }
+}
